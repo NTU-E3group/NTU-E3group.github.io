99aea0866ed666daa488ecd0760b4e3fdb02ec93
Yuan-Hsi Chien <hsi@caece.net>
2023-10-30 01:49:07 +0800

    101top, hp-window hover

--- js/script.js
+++ js/script.js
@@ -1,20 +1,29 @@
-// function change101Top() {
-//     let obj = document.querySelector(".hp-101");
-//     console.log(obj);
-// }
-// change101Top();
+const colorArray = ['--r-red', '--r-orange', '--r-yellow', '--r-green', '--r-blue', '-r-indigo', '--r-purple'];
 
+function change101Top() {
+    var timeNow = new Date();
+    var utc8Day = timeNow.getUTCHours() >= 16 ? timeNow.getUTCDay() + 1 : timeNow.getUTCDay();
+    var topColorHex = getComputedStyle(document.documentElement).getPropertyValue(colorArray[utc8Day % 7]);
+    document.querySelector(".fill-101-top").style.fill = topColorHex;
+};
+change101Top();
+
+var hpDiveIn = document.querySelector('.hp-dive-in');
+var hpTopLayer = document.querySelector('.hp-top-layer');
+
+hpTopLayer.addEventListener('mouseover', () => {
+    hpDiveIn.style.transform = 'translateY(-0.4375rem)';
+});
+hpTopLayer.addEventListener('mouseout', () => {
+    hpDiveIn.style.transform = 'translateY(0rem)';
+});
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
-}
+};
 
 var hpRainFront = document.querySelector('.hp-rain-front');
 var hpRainBack = document.querySelector('.hp-rain-back');
-
-
-
-
 
 function makeItRain(ifBack, rainSlope, precip, dropTime) {
     var hpSvgRain = ifBack ? document.querySelector('.hp-rain-back') : document.querySelector('.hp-rain-front');
@@ -34,14 +43,13 @@ function makeItRain(ifBack, rainSlope, precip, dropTime) {
         drops += `<line x1="${splatX + rainSlope}" y1="${ifBack ? splatY - 540 : 0}" x2="${splatX}" y2="${ifBack ? splatY : 540}" style="animation-delay: ${animationDelay / secDevide}s; animation-duration: ${dropTime / secDevide}s"/>`
             
         ifBack ? drops += `<path d="M${splatX - 10},${splatY}A7,5,0,0,1,${splatX + 10},${splatY}" style="transform-origin: ${splatX}px ${splatY}px; animation-delay: ${animationDelay / 1000}s; animation-duration: ${dropTime / 1000}s"/>` : null;
-    }
+    };
 
     hpSvgRain.innerHTML = drops;
-}
+};
 
 
 // dropTime in milliseconds (1000 - 2000)
-
 var rainSlope = 200;
 var precip = 1;
 var dropTime = 1300;
