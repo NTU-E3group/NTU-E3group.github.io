cf51b99412e569be0cd9270074d8c7992b706cb2
Yuan-Hsi Chien <hsi@caece.net>
2024-08-30 12:55:55 +0800

    modal for not supporting css nesting

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+// Check if the browser supports CSS nesting
+const supportsNesting = window.getComputedStyle(document.body, '::before').content.includes('css-nesting-supported');
+if (!supportsNesting) {
+    const nestingSupportDialog = document.querySelector('.nesting-support-dialog');
+    nestingSupportDialog.showModal(); // Show the dialog
+
+    document.querySelector('.nesting-support-dialog-close-btn').addEventListener('click', function() {
+        nestingSupportDialog.close(); // Close the dialog
+    });
+};
+
 const body = document.querySelector('body');
 
 function getRandomInt(min, max) {
