c527535de8990b47619f92ab9418303fbafa5111
Yuan-Hsi Chien <hsi@caece.net>
2024-09-11 21:17:44 +0800

    minor

--- js/script.js
+++ js/script.js
@@ -240,7 +240,25 @@ function glfSliderPush(push) {
 
     glfSliderBlock[glfSliderTo].style.setProperty('opacity', 1);
 
-}
+};
+
+// to top button
+var toTopBtn = document.querySelector('.to-top-btn');
+
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > window.innerHeight || document.documentElement.scrollTop > window.innerHeight) {
+        toTopBtn.style.display = "grid";
+        setTimeout(() => {
+            toTopBtn.style.opacity = "1";
+        }, 10);
+    } else {
+      toTopBtn.style.opacity = "0";
+        setTimeout(() => {
+            toTopBtn.style.display = "none";
+        }, 300);
+    };
+};
 
 // menu
 var menu = document.querySelector('.menu');
@@ -249,6 +267,7 @@ var menuCloseBtn = document.querySelector('.menu-close-btn');
 var menuBg = document.querySelector('.menu-bg');
 var menuA = document.querySelectorAll('.menu-a');
 
+// check if mobile
 window.mobileCheck = function() {
     let check = false;
     (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
@@ -265,8 +284,11 @@ function getScrollBarWidth() {
 };
 
 function menuOpen() {
+    // add scrollbar width to right margin so it won't shift
+    toTopBtn.style.marginInlineEnd = mobileCheck() ? 0 : getScrollBarWidth() + 'px';
     menuBtn.style.marginInlineEnd = mobileCheck() ? 0 : getScrollBarWidth() + 'px';
     body.style.paddingInlineEnd = mobileCheck() ? 0 : getScrollBarWidth() + 'px';
+
     body.classList.add('overflow-hidden');
     menu.classList.remove('display-none');
     setTimeout(() => {
@@ -275,8 +297,10 @@ function menuOpen() {
 };
 
 function menuClose() {
+    toTopBtn.style.marginInlineEnd = 0;
     menuBtn.style.marginInlineEnd = 0;
     body.style.paddingInlineEnd = 0;
+
     body.classList.remove('overflow-hidden');
     menu.classList.remove('menu-active');
     setTimeout(() => {
@@ -284,7 +308,6 @@ function menuClose() {
     }, 300);
 };
 
-// the bg part to be fixed
 [menuBtn, menuCloseBtn, menuBg].forEach(elem => {
     elem.addEventListener('click', (e) => {
         if (menu.classList.contains('menu-active')) {
@@ -306,23 +329,6 @@ menuA.forEach(elem => {
 //     console.log(e.target);
 // });
 
-// to top button
-var toTopBtn = document.querySelector('.to-top-btn');
-
-window.onscroll = function() {scrollFunction()};
-function scrollFunction() {
-    if (document.body.scrollTop > window.innerHeight || document.documentElement.scrollTop > window.innerHeight) {
-        toTopBtn.style.display = "grid";
-        setTimeout(() => {
-            toTopBtn.style.opacity = "1";
-        }, 10);
-    } else {
-      toTopBtn.style.opacity = "0";
-        setTimeout(() => {
-            toTopBtn.style.display = "none";
-        }, 300);
-    }
-  }
 
 // cursor
 let mouseX = 0, mouseY = 0;
