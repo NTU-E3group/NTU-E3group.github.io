b5bc973ec5e194d61d27e2254eca3dfe1c1820f3
Yuan-Hsi Chien <hsi@caece.net>
2024-09-13 15:16:09 +0800

    compress-img test mods

---
+++ testing-modules/compress-img/modules/compress-img.py
@@ -0,0 +1,47 @@
+from PIL import Image
+import glob
+import json
+
+# open json
+def open_json(file_path):
+    try:
+        # Open the JSON file for reading
+        with open(file_path, 'r', encoding='utf-8') as file:
+            # Load the JSON data
+            data = json.load(file)
+    
+        return data
+    
+    except FileNotFoundError:
+        print(f"Error: The file '{file_path}' does not exist.")
+    except json.JSONDecodeError as e:
+        print(f"Error decoding JSON: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+# Function to resize and convert an image to WebP
+def convert_to_webp(input_image_name, sizes, compression_quality=100):
+    # Get the input image path
+    input_image_path = glob.glob(f"{input_image_name}.*")[0]
+
+    """Converts the image to WebP format in multiple sizes"""
+    with Image.open(input_image_path) as img:
+        for size in sizes:
+            # Resize the image while maintaining aspect ratio
+            img_resized = img.resize((size, int(size * img.height / img.width)))
+            # Save the resized image in WebP format
+            webp_output_path = f"{input_image_path}-{size}w.webp"
+            img_resized.save(webp_output_path, "WEBP", quality=compression_quality)
+            print(f"Saved {webp_output_path} with size {size}px width")
+
+# Define desired sizes for WebP images (widths)
+ppl_img_sizes = [200, 400, 800]
+glf_img_sizes = [200, 400, 800, 1200, 1600, 2000]
+
+# Convert images to WebP format
+for ele in ['ppl', 'glf']:
+    data_list = open_json(f'../src/{ele}.json')
+
+    for data in data_list:
+        print(data['image_name'])
+        convert_to_webp(f"../glf/{data['image_name']}", globals()[f'{ele}_img_sizes'], compression_quality=100)
