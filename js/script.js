dd14d8ab79e713e9730c240283524af90422169d
Yuan-Hsi Chien <hsi@caece.net>
2024-09-24 22:07:57 +0800

    hp-plane right to left

--- js/script.js
+++ js/script.js
@@ -144,9 +144,9 @@ const hpPlaneText = document.querySelector('.hp-plane-text');
 
 
 hpPlaneTextBox.setAttribute('width', hpPlaneText.getBBox().width + 39);
-hpPlaneTextBox.setAttribute('x', -39 - hpPlaneText.getBBox().width);
+// hpPlaneTextBox.setAttribute('x', -39 - hpPlaneText.getBBox().width);
 hpPlaneTextBg.setAttribute('width', hpPlaneText.getBBox().width + 30);
-hpPlaneTextBg.setAttribute('x', -34.5 - hpPlaneText.getBBox().width);
+// hpPlaneTextBg.setAttribute('x', -34.5 - hpPlaneText.getBBox().width);
 
 // filters
 document.querySelectorAll('.filter').forEach((filter) => {
